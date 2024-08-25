9fd2dc9f36e8efdf3f45cea6f9707dc329409b03
Saikat-too <singhasaikat37@gmail.com>
2024-08-25 19:44:20 +0600

    final version of x_post_scraping

--- X_Scraper/X_Post_Scraper.py
+++ X_Scraper/X_Post_Scraper.py
@@ -1,37 +1,80 @@
 import requests
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By 
+class X_Post:
+    # Initializing the post_link 
+    def __init__(self ):
+        self.post_link = "https://x.com/ABrownBeing/status/1827381444894949685"
+        
+    def get_X_Post(self):
+        
+         # Boolean to check the link is valid or not 
+        bool = True
+        try : 
+            r = requests.get(self.post_link , timeout=1)
+            if r.status_code < 400 : 
+                bool = True
+            else : 
+                bool = False
+        except ConnectionError:
+            bool = False
+        
+        # Break the execution if the link is invalid 
+        if not bool : 
+            print(f"Invalid URL : {self.post_link}")
+            return 
+        
+        # Open the Firefox browser and setting up the driver       
+        driver = webdriver.Firefox()
 
-driver = webdriver.Firefox()
+        # Getting the link of the desirable post
+        driver.get(self.post_link)
 
-url = "https://x.com/ABrownBeing/status/1827381444894949685"
+        # Getting the text of the post
+        texts = driver.title 
 
-driver.get(url)
+        # Initializing a variable that will help us to differentiate between Author and post
+        i = 0 
 
-texts = driver.title 
+        # String for getting the author name
+        id = " "
 
-i = 0 
+        # String for getting the post 
+        post = " "
 
-id = " "
+        # Iterating the  text of post which is in actually character array 
+        for text in texts:
+            # Initializing i value for not taking character to post string
+            if text=="X":
+                i = 1 
+            # i == 0 mean that the characters are part of the author name 
+            if i == 0 : 
+                id+=text
+            # if not then the characters are part of  post and avoid the character X 
+            elif i == 1 and text != "X":
+                post+=text
+                
 
-post = " "
+        # Using re to remove some unnecessary char 
+        post = re.sub(r'[":/]' ,'' ,post)
+        # Striping post and id string 
+        id.strip()
 
-for text in texts:
-    if text=="X":
-        i = 1 
-    if text == ":" or text == " / " or text == "on":
-        pass
-    if i == 0 : 
-        id+=text
-    elif i == 1 and text != "X":
-        post+=text
-         
+        post.strip()
 
-# print(driver.title)
-id.strip()
+        # Replacing unncecessary words 
+        id = id.rstrip(id[-3:])
+        
+        driver.quit()
+        
+        return {
+            "Author" : id , 
+            "Post" : post 
+        }
+        
+        
+        
 
-post.strip()
 
-print(id)
-print(post)
-driver.quit()
+        
