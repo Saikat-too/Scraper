e5028b0d4a536ae98698b85cd1aabc3b47daf137
Saikat-too <singhasaikat37@gmail.com>
2024-08-25 18:19:04 +0600

    sucessfully did the initial post scraping for X

--- X_Scraper/X_Post_Scraper.py
+++ X_Scraper/X_Post_Scraper.py
@@ -4,8 +4,34 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Firefox()
 
-url = "https://x.com/kareemmpie/status/1827336078551838962"
+url = "https://x.com/ABrownBeing/status/1827381444894949685"
 
 driver.get(url)
 
-print(driver.title)
+texts = driver.title 
+
+i = 0 
+
+id = " "
+
+post = " "
+
+for text in texts:
+    if text=="X":
+        i = 1 
+    if text == ":" or text == " / " or text == "on":
+        pass
+    if i == 0 : 
+        id+=text
+    elif i == 1 and text != "X":
+        post+=text
+         
+
+# print(driver.title)
+id.strip()
+
+post.strip()
+
+print(id)
+print(post)
+driver.quit()
